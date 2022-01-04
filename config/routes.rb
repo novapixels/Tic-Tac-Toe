c68268f2d1936698ea9878d45b5265e910c6617f
monica randriamialy <mo@novapixels.net>
2022-01-04 19:41:00 +0100

    game update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'games/index'
-  get 'games/show'
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :games
+  root to: 'games#index'
 end
